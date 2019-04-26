21198a4448d33deb31ef57cc9d3286547456ee0f
mgalanti <mario.galanti@cern.ch>
2019-04-26 11:46:02 +0200

    Create MGSelector.h

---
+++ Ntu/interface/MGSelector.h
@@ -0,0 +1,30 @@
+#ifndef MGSelector_H
+#define MGSelector_H
+
+#include "CommonTools/Statistics/interface/ChiSquaredProbability.h"
+
+#include "PDAnalysis/Ntu/interface/MGRecoTools.h"
+#include "PDAnalysis/Ntu/bin/PDAnalyzerUtil.h"
+
+class MGSelector: public virtual PDAnalyzerUtil, public virtual MGRecoTools
+{  
+  public:
+    
+    MGSelector();
+    virtual ~MGSelector();
+    
+    bool SelectBsToJPsiPhiEvent(const std::string selection, std::vector<std::pair<int,int> >& selectedObjects);
+    
+    bool SelectHlt(const std::string selection);
+    bool SelectElectron(const int iEle, const std::string selection);
+    bool SelectJPsi(const int iJPsi, const std::string selection);
+    bool SelectPhi(const int iPhi, const std::string selection);
+    
+    int SelectBestBsToJPsiPhi(const std::string selection, int& iPV);
+    int SelectBestPV(const int iSvt, const TLorentzVector& pSvt, const std::string selection);
+    
+  private:
+    // Nothing at the moment
+};
+
+#endif
