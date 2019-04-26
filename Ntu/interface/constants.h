cec372e8f62e967a80581877adb7dbf193e2f48a
mgalanti <mario.galanti@cern.ch>
2019-04-26 11:43:51 +0200

    Create constants.h

---
+++ Ntu/interface/constants.h
@@ -0,0 +1,24 @@
+#ifndef constants_h
+#define constants_h
+
+
+
+namespace constants
+{
+  // Mathematical constants
+  const double PI = 3.141592653589793;
+
+  // Particle properties
+  const double muonMass = 0.1057;
+  const double pionMass = 0.1396;
+  const double kaonMass = 0.4937;
+  const double jPsiMass = 3.097;
+  const double phiMass = 1.019;
+  const double protonMass = 0.9382;
+  const double lambdaMass = 1.116;
+  const double BsMass = 5.3663;
+}
+
+
+
+#endif // constants_h
