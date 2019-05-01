fabde94104c6f705ff2e3b9618db705fca1949da
Mario Galanti <mario.galanti@cern.ch>
2019-05-01 12:07:46 +0200

    Add Ntuplizer to recognized names

--- CreateNewAnalyzer.sh
+++ CreateNewAnalyzer.sh
@@ -15,10 +15,10 @@ if [[ $1 == '' || $2 != '' ]]
 fi
 
 # Some common words that can be present in the analyzer name, to use in string substitution below
-declare -a -r MainStrings=("Analyzer" "Reader" "Writer" "Producer")
-# declare -a -r LCStrings=("analyzer" "reader" "writer" "producer")
-declare -a -r VerbStrings=("analyze" "read" "write" "produce")
-declare -a -r NounStrings=("Analysis" "Reading" "Writing" "Production")
+declare -a -r MainStrings=("Analyzer" "Reader" "Writer" "Producer" "Ntuplizer")
+# declare -a -r LCStrings=("analyzer" "reader" "writer" "producer" "ntuplizer")
+declare -a -r VerbStrings=("analyze" "read" "write" "produce" "ntuplize")
+declare -a -r NounStrings=("Analysis" "Reading" "Writing" "Production" "Ntuplization")
 
 name=$1
 LCName=${name,}
