0ddc122e1b8db1b009d4c2d9e464f174241f91a4
Mario Galanti <mario.galanti@cern.ch>
2019-06-06 16:26:10 +0200

    Update selection to latest prescriptions

--- Ntu/src/MGSelector.cc
+++ Ntu/src/MGSelector.cc
@@ -106,21 +106,7 @@ bool MGSelector::SelectEvent(const std::string selection, std::vector<std::pair<
 
 bool MGSelector::SelectBsToJPsiPhiEvent(const std::string selection, std::vector<std::pair<int,int> >& selectedObjectsRef)
 {
-  if(selection.compare("eleTagLooseV0") == 0)
-  {
-    if(SelectHlt("eleTagHLTV0") == false)
-      return false;
-    int iPV = -1;
-    int bestBsToJPsiPhi = SelectBestBsToJPsiPhi("LooseV0", iPV);
-    if(bestBsToJPsiPhi < 0) return false;
-    if(iPV < 0) return false;
-    std::pair<int,int> selObject = std::make_pair(PDEnumString::recSvt, bestBsToJPsiPhi);
-    selectedObjectsRef.push_back(selObject);
-    selObject = std::make_pair(PDEnumString::recPV, iPV);
-     selectedObjectsRef.push_back(selObject);
-     return true;
-  }
-  else if(selection.compare("eleTagTightV1") == 0 || selection.compare("eleTagTightV0") == 0)
+  if(selection.compare("eleTagTightV2") == 0 || selection.compare("eleTagTightV1") == 0 || selection.compare("eleTagTightV0") == 0)
   {
     if(SelectHlt("eleTagHLTV0") == false)
       return false;
@@ -133,6 +119,20 @@ bool MGSelector::SelectBsToJPsiPhiEvent(const std::string selection, std::vector
     selObject = std::make_pair(PDEnumString::recPV, iPV);
     selectedObjectsRef.push_back(selObject);
     return true;
+  }
+  else if(selection.compare("eleTagLooseV0") == 0)
+  {
+    if(SelectHlt("eleTagHLTV0") == false)
+      return false;
+    int iPV = -1;
+    int bestBsToJPsiPhi = SelectBestBsToJPsiPhi("LooseV0", iPV);
+    if(bestBsToJPsiPhi < 0) return false;
+    if(iPV < 0) return false;
+    std::pair<int,int> selObject = std::make_pair(PDEnumString::recSvt, bestBsToJPsiPhi);
+    selectedObjectsRef.push_back(selObject);
+    selObject = std::make_pair(PDEnumString::recPV, iPV);
+     selectedObjectsRef.push_back(selObject);
+     return true;
   }
   std::cout << "MGSelector::SelectBsToJPsiPhiEvent(): W A R N I N G ! Reached default return statement. Event will not be selected...\n";
   std::cout << "                                      The selection string passed to this method is: \"" << selection << "\"\n";
@@ -193,7 +193,10 @@ int MGSelector::SelectOSElectron(const std::string selection, const int iPV, con
   if (nElectrons == 0)
     return -1;
   
-  if(selection.compare("eleTagTightV1") == 0 || selection.compare("eleTagTightV0") == 0 || selection.compare("eleTagLooseV0") == 0)
+  if(selection.compare("eleTagTightV2") == 0 || 
+     selection.compare("eleTagTightV1") == 0 || 
+     selection.compare("eleTagTightV0") == 0 || 
+     selection.compare("eleTagLooseV0") == 0)
   {
     std::vector <int> tkSsB = tracksFromSV(iB);
     TLorentzVector tB = GetTLorentzVectorFromJPsiX(iB);
@@ -254,7 +257,37 @@ int MGSelector::SelectOSElectron(const std::string selection, const int iPV, con
 
 bool MGSelector::SelectJPsi(const int iJPsi, const std::string selection)
 {
-  if(selection.compare("TightV0") == 0) // This is the same as IsTightJPsi() in Alberto code.
+  if(selection.compare("TightV1") == 0)
+  {
+    if(fabs(svtMass->at(iJPsi) - constants::jPsiMass) > 0.15)
+    {
+      return false;
+    }
+    
+    std::vector<int> tkJPsi = tracksFromSV(iJPsi);
+    TLorentzVector tJPsi(0,0,0,0);
+
+    for(uint i=0; i < tkJPsi.size(); i++)
+    {
+      int j = tkJPsi[i];
+      if(trkPt->at(j) < 4.0) // For JPsiTrktrk
+      {
+        return false;
+      }
+      if(fabs(trkEta->at(j)) > 2.5)
+      {
+        return false;
+      }
+      TLorentzVector a;
+      a.SetPtEtaPhiM(trkPt->at(j), trkEta->at(j), trkPhi->at(j), constants::muonMass);
+      tJPsi += a;
+    }
+
+    // if(tJPsi.Pt() < 7.0) return false;
+
+    return true;    
+  }
+  else if(selection.compare("TightV0") == 0) // This is the same as IsTightJPsi() in Alberto code.
   {
     if(fabs(svtMass->at(iJPsi) - constants::jPsiMass) > 0.15)
     {
@@ -292,7 +325,35 @@ bool MGSelector::SelectJPsi(const int iJPsi, const std::string selection)
 
 bool MGSelector::SelectPhi(const int iPhi, const std::string selection)
 {
-  if(selection.compare("TightV0") == 0) // This is the same as IsTightJPsi() in Alberto code.
+  if(selection.compare("TightV1") == 0)
+  {
+    if(fabs(svtMass->at(iPhi) - constants::phiMass) > 0.01)
+    {
+      return false;
+    }
+    
+    std::vector<int> tkPhi = tracksFromSV(iPhi);
+    for(uint i=0; i < tkPhi.size(); i++)
+    {
+      int j = tkPhi[i];
+      if(trkPt->at(j) < 0.8) // For JpsiTrkTrk
+      {
+        return false;
+      }
+      if(fabs(trkEta->at(j)) > 2.5)
+      {
+        return false;
+      }
+
+      // FIXME: MG: write a method inside MGRecoTools that does this automatically...
+      int kaonHits = trkHitPattern->at(tkPhi[i]);
+      kaonHits = (int(kaonHits) / 100) % 10000;
+      kaonHits = kaonHits % 100;
+      if(kaonHits < 4) return false;
+    }
+    return true;    
+  }
+  else if(selection.compare("TightV0") == 0) // This is the same as IsTightJPsi() in Alberto code.
   {
     if(fabs(svtMass->at(iPhi) - constants::phiMass) > 0.01)
     {
@@ -350,46 +411,86 @@ int MGSelector::SelectBestBsToJPsiPhi(const std::string selection, int& iBestPV)
   iBestPV = -1;
   TLorentzVector tB(0,0,0,0);
   
-  if(selection.compare("LooseV0") == 0) // This is the same as GetBestBstrange() in Alberto code.
+  if(selection.compare("TightV2") == 0)
   {
     int index = -1;
-    float bestChi2 = 1e9;
+    float best = 0.;
+    
     for(int iB = 0; iB < nSVertices; iB++)
     {
-       if((svtType->at(iB) != PDEnumString::svtBsJPsiPhi))
-       {
-         continue;
-       }
-       if(svtMass->at(iB) < 5.0 || svtMass->at(iB) > 6.0)
-       {
-         continue;
-       }
-       if(svtChi2->at(iB) > bestChi2)
-       {
-         continue;
-       }
-       
-       std::vector<int> tkSsB = tracksFromSV(iB);
-       int iJPsi = (subVtxFromSV(iB)).at(0);
-       std::vector<int> tkJpsi = tracksFromSV(iJPsi);
-
-       for(uint i = 0; i < tkSsB.size(); i++)
-       {
-         int j = tkSsB[i];
-         float m = constants::kaonMass;
-         if(j == tkJpsi[0] || j == tkJpsi[1])
-         {
-           m = constants::muonMass;
-         }
-         TLorentzVector a;
-         a.SetPtEtaPhiM(trkPt->at(j), trkEta->at(j), trkPhi->at(j), m);
-         tB += a;
-       }
-       iBestPV = SelectBestPV(iB, tB, "PointingV0");
-       index = iB;
-       bestChi2 = svtChi2->at(iB);
+      if((svtType->at(iB) != PDEnumString::svtBsJPsiPhi))
+      {
+        continue;
+      }
+      
+      int iJPsi = (subVtxFromSV(iB)).at(0);
+      int iPhi = (subVtxFromSV(iB)).at(1);
+      
+      std::vector<int> tkSsB = tracksFromSV(iB);
+      std::vector<int> tkJpsi = tracksFromSV(iJPsi);
+      std::vector<int> tkPhi = tracksFromSV(iPhi);
+      
+      //JPsi
+      if(!SelectJPsi(iJPsi, "TightV1"))
+      {
+        continue;
+      }
+      //Phi
+      if(!SelectPhi(iPhi, "TightV1"))
+      {
+        continue;
+      }
+      
+      for(uint i = 0; i < tkSsB.size(); i++)
+      {
+        int j = tkSsB[i];
+        float m = constants::kaonMass;
+        if(j == tkJpsi[0] || j == tkJpsi[1])
+        {
+          m = constants::muonMass;
+        }
+        TLorentzVector a;
+        a.SetPtEtaPhiM(trkPt->at(j), trkEta->at(j), trkPhi->at(j), m);
+        tB += a;
+      }
+      
+      //BS
+      float bVprob = ChiSquaredProbability(svtChi2->at(iB), svtNDOF->at(iB));
+      if(svtMass->at(iB) < 5.2 || svtMass->at(iB) > 5.65)
+      {
+        continue;
+      }
+      if(bVprob < 0.02)
+      {
+        continue;
+      }
+//       if(tB.Pt() < 10.)
+//       {
+//         continue;
+//       }
+      
+      int iPV = SelectBestPV(iB, tB, "PointingV0");
+      
+      if(iPV < 0)
+      {
+        continue;
+      }
+      
+      float ct2DCut = 0.02;
+      if(GetCt2D(tB, iB) < ct2DCut)
+      {
+        continue;
+      }
+      
+      if(bVprob < best)
+      {
+        continue;
+      }
+      iBestPV = iPV;
+      index = iB;
+      best = bVprob;
     }
-    return index;
+    return index;    
   }
   else if(selection.compare("TightV1") == 0 || selection.compare("TightV0") == 0) // This is the same as GetBestBstrangeTight() in Alberto code.
   {
@@ -469,6 +570,47 @@ int MGSelector::SelectBestBsToJPsiPhi(const std::string selection, int& iBestPV)
       iBestPV = iPV;
       index = iB;
       best = bVprob;
+    }
+    return index;
+  }
+  else if(selection.compare("LooseV0") == 0) // This is the same as GetBestBstrange() in Alberto code.
+  {
+    int index = -1;
+    float bestChi2 = 1e9;
+    for(int iB = 0; iB < nSVertices; iB++)
+    {
+       if((svtType->at(iB) != PDEnumString::svtBsJPsiPhi))
+       {
+         continue;
+       }
+       if(svtMass->at(iB) < 5.0 || svtMass->at(iB) > 6.0)
+       {
+         continue;
+       }
+       if(svtChi2->at(iB) > bestChi2)
+       {
+         continue;
+       }
+       
+       std::vector<int> tkSsB = tracksFromSV(iB);
+       int iJPsi = (subVtxFromSV(iB)).at(0);
+       std::vector<int> tkJpsi = tracksFromSV(iJPsi);
+
+       for(uint i = 0; i < tkSsB.size(); i++)
+       {
+         int j = tkSsB[i];
+         float m = constants::kaonMass;
+         if(j == tkJpsi[0] || j == tkJpsi[1])
+         {
+           m = constants::muonMass;
+         }
+         TLorentzVector a;
+         a.SetPtEtaPhiM(trkPt->at(j), trkEta->at(j), trkPhi->at(j), m);
+         tB += a;
+       }
+       iBestPV = SelectBestPV(iB, tB, "PointingV0");
+       index = iB;
+       bestChi2 = svtChi2->at(iB);
     }
     return index;
   }
